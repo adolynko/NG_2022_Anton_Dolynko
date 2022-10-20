8ecbc131afe8b25e6396fc6282448b8095fd1b6c
adolynko <dolynkoanton@gmail.com>
2022-10-20 20:15:02 +0300

    added task 4

---
+++ Lesson_1/task4.py
@@ -0,0 +1,12 @@
+a,b,c = int(input()),int(input()),int(input())
+#ax^2+bx+c=0
+d=b**2-4*a*c
+if d<0:
+  print("there are no solutions")
+elif d == 0:
+    print(-b / (2*a))
+elif d > 0:
+    x1 = (-b - d ** 0.5) / (2*a)
+    x2 = (-b + d ** 0.5) / (2*a)
+    print(x1)
+    print(x2)

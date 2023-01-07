4e8f5dd40e607274761cfd75f5a7e06f7e6a7486
adolynko <dolynkoanton@gmail.com>
2023-01-07 12:55:43 +0200

    added task1

--- Lesson_4/task1.py
+++ Lesson_4/task1.py
@@ -1,20 +1,40 @@
 import psutil
-import time
+import platform
 
-print(1, psutil.cpu_freq())#частота процессора
+logicvalue = ['n','n','n','n','n']
+cpu_freq = psutil.cpu_freq()
+logical_cpus = psutil.cpu_count()
+cpu_name = platform.processor()
+os_name = platform.platform()
+RAM = psutil.virtual_memory()
+entries = list([str(cpu_freq[2]), str(logical_cpus), cpu_name, os_name, str(RAM[0]) ])
+entriesForReading = ["cpu_freq", "logical_cpus", "cpu_name","os_name","RAM(in bytes)"]
+def ShowMenu():
+    for entry in entries:
+        print('[',entries.index(entry),'] / [', logicvalue[entries.index(entry)], ']:', entriesForReading[entries.index(entry)] )
+    print(len(entries),'proceed :')
 
-print(2, psutil.cpu_count(logical=False)) #ядра
-print(3 ,psutil.cpu_count(logical=True)) #логические процессоры
-################################
-print(4, psutil.virtual_memory( ))#оперативная память
-print(5, psutil.swap_memory( ))#подкачка
-################################
-print(6, psutil.disk_partitions(all=False))
-print(7, psutil.disk_usage('C:\\'))
-print(8, psutil.disk_io_counters( perdisk=False, nowrap=True ))
-################################
+def WriteInTxt():
+    info = open('setup.txt', 'a')
+    for entry in entries:
+        if logicvalue[entries.index(entry)] == 'y':
+            info.write(f'{entry}\n')
+    info.close()
 
+def main():
+    ShowMenu()
+    while True:
+        print('Choose option')
+        data = int(input())
+        if data == len(logicvalue):
+            break
+        if logicvalue[data] == 'n':
+            logicvalue[data] = 'y'
+        else:
+            logicvalue[data] = 'n'
+        ShowMenu()   
+    print('finished')
+    WriteInTxt()
 
-
-
-#psutil.cpu_count
+main()
+        

92d517936ff6da5b14e55f87c8aee4c0f2b2d561
adolynko <dolynkoanton@gmail.com>
2023-01-25 10:42:01 +0200

    replaced srting 'y' and 'n' to boolean

--- Lesson_4/task1.py
+++ Lesson_4/task1.py
@@ -1,7 +1,6 @@
 import psutil
 import platform
 
-logicvalue = ['n','n','n','n','n']
 cpu_freq = psutil.cpu_freq()
 logical_cpus = psutil.cpu_count()
 cpu_name = platform.processor()
@@ -9,32 +8,39 @@ os_name = platform.platform()
 RAM = psutil.virtual_memory()
 entries = list([str(cpu_freq[2]), str(logical_cpus), cpu_name, os_name, str(RAM[0]) ])
 entriesForReading = ["cpu_freq", "logical_cpus", "cpu_name","os_name","RAM(in bytes)"]
-def ShowMenu():
-    for entry in entries:
-        print('[',entries.index(entry),'] / [', logicvalue[entries.index(entry)], ']:', entriesForReading[entries.index(entry)] )
-    print(len(entries),'proceed :')
 
-def WriteInTxt():
-    info = open('setup.txt', 'a')
-    for entry in entries:
-        if logicvalue[entries.index(entry)] == 'y':
-            info.write(f'{entry}\n')
-    info.close()
+def logicalToStrConverter(logic):
+    return "y" if logic else "n"
 
-def main():
-    ShowMenu()
+def showMenu(modes):
+    for entry in entries:
+        print("[" + str(entries.index(entry)) + "] / [ " + logicalToStrConverter(modes[entry]) + " ]: " + str(entriesForReading[entries.index(entry)]))
+    print("["+ str(len(entries)) + "]: Proceed")
+
+def getUserChoice(modes):
+    showMenu(modes)
+    return int(input("Choose the option "))
+
+def buildModes():
+    modes = {}
+    for entry in entries:
+        modes[entry] = False
+    return modes
+
+def startManager():
+    modes = buildModes()
     while True:
-        print('Choose option')
-        data = int(input())
-        if data == len(logicvalue):
+        choice = getUserChoice(modes)
+        if choice == len(entries):
             break
-        if logicvalue[data] == 'n':
-            logicvalue[data] = 'y'
         else:
-            logicvalue[data] = 'n'
-        ShowMenu()   
-    print('finished')
-    WriteInTxt()
+            modes[entries[choice]] = not modes[entries[choice]]
+    writeInTxt(modes)
 
-main()
-        
+def writeInTxt(modes):
+    info = open("info.txt","w")
+    for values in modes:
+        if logicalToStrConverter(modes[values]) == "y":
+            info.write(f'{values}\n')
+
+startManager()

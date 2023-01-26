816575542f27828bddf355e6235feb096ba0738b
adolynko <dolynkoanton@gmail.com>
2023-01-26 22:18:39 +0200

    renamed variables

--- We know/bank_card.py
+++ We know/bank_card.py
@@ -4,13 +4,13 @@ def validity(cardNumber):
         list = ""
         for lenthCardNumber in range(8):
             list = list + str(int(cardNumber[-2 - 2 * lenthCardNumber]) * 2)
-        list1 = 0
-        for everyValueInList in list:
-            list1 += int(everyValueInList)
-        list2 = 0
-        for i in range(8):
-            list2 = list2 + int(cardNumber[-1 - 2 * i])
-        if (int(list1) + list2) % 10 == 0:
+        halfcard = 0
+        for values in list:
+            halfcard += int(values)
+        fullcard = 0
+        for values in range(8):
+            fullcard = fullcard + int(cardNumber[-1 - 2 * values])
+        if (int(halfcard) + fullcard) % 10 == 0:
             identification(cardNumber[0], cardNumber[1])
         else:
             print('Invalid')
@@ -20,15 +20,15 @@ def validity(cardNumber):
         for lenthCardNumber in range(7):
             list = list + str(int(cardNumber[-2 - 2 * lenthCardNumber]) * 2)
         print(list)
-        list1 = 0
-        for everyValueInList in list:
-            list1 += int(everyValueInList)
-        print(list1)
-        list2 = 0
-        for i in range(8):
-            list2 = list2 + int(cardNumber[-1 - 2 * i])
-        print(list2)
-        if (int(list1) + list2) % 10 == 0:
+        halfcard = 0
+        for values in list:
+            halfcard += int(values)
+        print(halfcard)
+        fullcard = 0
+        for values in range(8):
+            fullcard = fullcard + int(cardNumber[-1 - 2 * values])
+        print(fullcard)
+        if (int(halfcard) + fullcard) % 10 == 0:
             identification(cardNumber[0], cardNumber[1])
         else:
             print('Invalid')
@@ -38,24 +38,24 @@ def validity(cardNumber):
         for lenthCardNumber in range(6):
             list = list + str(int(cardNumber[-2 - 2 * lenthCardNumber]) * 2)
         print(list)
-        list1 = 0
-        for everyValueInList in list:
-            list1 += int(everyValueInList)
-        print(list1)
-        list2 = 0
-        for i in range(7):
-            list2 = list2 + int(cardNumber[-1 - 2 * i])
-        print(list2)
-        if (int(list1) + list2) % 10 == 0:
+        halfcard = 0
+        for values in list:
+            halfcard += int(values)
+        print(halfcard)
+        fullcard = 0
+        for values in range(7):
+            fullcard = fullcard + int(cardNumber[-1 - 2 * values])
+        print(fullcard)
+        if (int(halfcard) + fullcard) % 10 == 0:
             identification(cardNumber[0], cardNumber[1])
         else:
             print('Invalid')
 
 def identification(firstCardNumber, secondCardNumber):
-    a = firstCardNumber + secondCardNumber
+    numbers = firstCardNumber + secondCardNumber
     mastercard = ("51", "52", "53", "54", "55")
     amex = ("34", "37")
-    if a in mastercard:
+    if numbers in mastercard:
         print("MasterCard")
     elif identification in amex:
         print("AmEx")
